6881b3d4274df65e905d5b1c6f233b36661c88e7
Bentley <bentleyturner@live.com>
2024-11-26 21:29:05 +1100

    Adding first frontend tests

--- WeatherFetcherWeb.Tests/UnitTest1.cs
+++ WeatherFetcherWeb.Tests/UnitTest1.cs
@@ -1,10 +1,38 @@
-﻿namespace WeatherFetcherWeb.Tests;
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System.Threading.Tasks;
+using Xunit;
 
-public class UnitTest1
+namespace WeatherFetcherWeb.Tests
 {
-    [Fact]
-    public void Test1()
+    public class IndexPageTests : IClassFixture<WebApplicationFactory<Program>>
     {
+        private readonly HttpClient _client;
 
+        public IndexPageTests(WebApplicationFactory<Program> factory)
+        {
+            _client = factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task Get_IndexPage_ReturnsSuccessStatusCode()
+        {
+            // Act
+            var response = await _client.GetAsync("/");
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+        }
+
+        [Fact]
+        public async Task Get_IndexPage_ReturnsExpectedContent()
+        {
+            // Act
+            var response = await _client.GetAsync("/");
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            var content = await response.Content.ReadAsStringAsync();
+            Assert.Contains("Welcome", content); 
+        }
     }
 }
