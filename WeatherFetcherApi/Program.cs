e436593e086ad8aaacab8107bdd6358c5152827a
Bentley <bentleyturner@live.com>
2024-11-26 21:09:54 +1100

    Some more renaming and init the frontend and frontend test projects

--- WeatherFetcherApi/Program.cs
+++ WeatherFetcherApi/Program.cs
@@ -1,3 +1,4 @@
+using AspNetCoreRateLimit;
 using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,10 +9,18 @@ builder.Services
     .AddEndpointsApiExplorer()
     .AddSwaggerGen();
 
+
+builder.Services.AddMemoryCache();
+builder.Services.Configure<ClientRateLimitOptions>(builder.Configuration.GetSection("ClientRateLimiting"));
+builder.Services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
+builder.Services.AddInMemoryRateLimiting();
+
+
 var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI();
+app.UseClientRateLimiting();
 
 app.MapGet("/weatherdescription",
     (string cityName, string countryName, string apiKey, [FromServices] IWeatherService weatherService) =>
---
+++ WeatherFetcherWeb.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace WeatherFetcherWeb.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
